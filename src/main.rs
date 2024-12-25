7651bd238b4afd5ec4990da8ed0f75545898e3a4
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2024-12-25 20:00:30 +0100

    print generated board

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
 use itertools::Itertools;
 use rand::prelude::*;
-use std::collections::HashSet;
+use std::{
+    collections::HashSet,
+    fmt::Debug,
+};
 
 fn main() {
     let board = gen_board(10, 10, 10);
-    assert_eq!(board.len(), 100)
+    assert_eq!(board.len(), 100);
+    println!("{:?}", board)
 }
 
 struct Tile {
     pub pos: (i32, i32),
     pub is_mine: bool,
     pub mine_neighbors: u8,
+}
+
+impl Debug for Tile {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Tile")
+            .field("pos", &self.pos)
+            .field("is_mine", &self.is_mine)
+            .field("mine_neighbors", &self.mine_neighbors)
+            .finish()
+    }
 }
 
 fn gen_board(width: i32, height: i32, mines: i32) -> Vec<Tile> {
