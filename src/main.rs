247263e9ff6c88fcc2a892cac1967b1620d1cfe4
Pascal Ellenberger <56129985+rijkt@users.noreply.github.com>
2024-12-25 21:24:27 +0100

    generate board (#1)
    * add rand crate
    
    * generate mine coordinates
    
    * initialize the board
    
    * initialize with mines
    
    * fix: use exclusive ranges
    
    * wip: simplify model
    
    * extract fn
    
    * wip: single pass build
    
    * add itertools
    
    * cleanup
    
    * print generated board
    
    * wip: eagerly count neighboring mines
    
    * count neighbors by comparing distance to mines
    
    * extract module

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,7 @@
+mod board;
+
 fn main() {
-    let mine = Mine {
-        x: 0,
-        y: 0
-    };
-
-    assert!(matches!(mine.check(), CheckResult::BOOM))
-}
-
-enum CheckResult {
-    OK,
-    BOOM
-}
-
-trait Checkable {
-    fn check(&self) -> CheckResult;
-}
-
-struct Mine {
-    pub x: i32,
-    pub y: i32
-}
-
-struct NumberTile {
-    pub x: i32,
-    pub y: i32,
-    pub mine_neighbors: i32
-}
-
-impl Checkable for Mine {
-    fn check(&self) -> CheckResult {
-        CheckResult::BOOM
-    }
-}
-
-impl Checkable for NumberTile {
-    fn check(&self) -> CheckResult {
-        CheckResult::OK
-    }
+    let board = board::gen_board(10, 10, 10);
+    assert_eq!(board.len(), 100);
+    println!("{:?}", board)
 }
