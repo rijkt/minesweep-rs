5b1681c5832c4119c1ed31001fea3fdde5f5084d
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2024-12-25 16:46:11 +0100

    generate mine coordinates

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
-fn main() {
-    let mine = Mine {
-        x: 0,
-        y: 0
-    };
+use rand::prelude::*;
+use std::{collections::HashSet, vec};
 
-    assert!(matches!(mine.check(), CheckResult::BOOM))
+fn main() {
+    let mine = Mine { x: 0, y: 0 };
+
+    assert!(matches!(mine.check(), CheckResult::BOOM));
+    gen_board(10, 10, 10)
 }
 
 enum CheckResult {
     OK,
-    BOOM
+    BOOM,
 }
 
 trait Checkable {
@@ -18,13 +19,13 @@ trait Checkable {
 
 struct Mine {
     pub x: i32,
-    pub y: i32
+    pub y: i32,
 }
 
 struct NumberTile {
     pub x: i32,
     pub y: i32,
-    pub mine_neighbors: i32
+    pub mine_neighbors: i32,
 }
 
 impl Checkable for Mine {
@@ -37,4 +38,13 @@ impl Checkable for NumberTile {
     fn check(&self) -> CheckResult {
         CheckResult::OK
     }
+}
+
+fn gen_board(width: i32, height: i32, mines: i32) {
+    let mut rng = rand::thread_rng();
+
+    let points: HashSet<(i32, i32)> = (0..mines)
+        .map(|_| (rng.gen_range(0..=width), rng.gen_range(0..=height)))
+        .collect();
+    print!("{:?}", points)
 }
