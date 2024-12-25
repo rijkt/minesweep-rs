e0047e6fae54c8ff2d167223dfe2127ab2e33e36
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2024-12-25 19:23:54 +0100

    wip: simplify model

--- src/main.rs
+++ src/main.rs
@@ -2,64 +2,22 @@ use rand::prelude::*;
 use std::{collections::HashSet, vec};
 
 fn main() {
-    let mine = Mine { x: 0, y: 0 };
-
-    assert!(matches!(mine.check(), CheckResult::BOOM));
     gen_board(10, 10, 10)
 }
 
-enum CheckResult {
-    OK,
-    BOOM,
-}
-
-trait Checkable {
-    fn check(&self) -> CheckResult;
-}
-
-struct Mine {
-    pub x: i32,
-    pub y: i32,
-}
-
-impl Clone for Mine {
-    fn clone(&self) -> Self {
-        Mine {
-            x: self.x,
-            y: self.y
-        }
-    }
-}
-
-struct NumberTile {
-    pub x: i32,
-    pub y: i32,
+struct Tile {
+    pub pos: (i32, i32),
+    pub is_mine: bool,
     pub mine_neighbors: u8,
 }
 
-impl Checkable for Mine {
-    fn check(&self) -> CheckResult {
-        CheckResult::BOOM
-    }
-}
-
-impl Checkable for NumberTile {
-    fn check(&self) -> CheckResult {
-        CheckResult::OK
-    }
-}
-
-struct InitializingTile {}
-
-impl Checkable for InitializingTile {
-    fn check(&self) -> CheckResult {
-        panic!("Encountered initializing tile")
-    }
-}
-
-impl Clone for InitializingTile {
+impl Clone for Tile {
     fn clone(&self) -> Self {
-        InitializingTile {}
+        Tile {
+            pos: self.pos,
+            is_mine: self.is_mine,
+            mine_neighbors: self.mine_neighbors,
+        }
     }
 }
 
@@ -68,12 +26,12 @@ fn gen_board(width: i32, height: i32, mines: i32) {
     let mine_coords: HashSet<(i32, i32)> = (0..mines)
         .map(|_| (rng.gen_range(0..width), rng.gen_range(0..height)))
         .collect();
-    let init = Mine {x: -1, y: -1};
-    let mut board: Vec<Vec<Mine>> = vec![vec![init; width as usize]; height as usize];
-    mine_coords
-        .iter()
-        .for_each(|(x, y)| board[*x as usize][*y as usize] = Mine{
-            x: *x,
-            y: *y
-        });
+    let mut board: Vec<Vec<Tile>> = vec![Vec::with_capacity(width as usize); height as usize];
+    mine_coords.iter().for_each(|(x, y)| {
+        board[*x as usize][*y as usize] = Tile {
+            pos: (*x, *y),
+            is_mine: true,
+            mine_neighbors: 0, // TODO
+        }
+    });
 }
