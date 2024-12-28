3d1a9e65aef54b6727146622b7fe692a210aa93b
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2024-12-28 18:02:25 +0100

    validate input

--- src/main.rs
+++ src/main.rs
@@ -15,31 +15,39 @@ fn main() {
 
     renderer.render(&controller.state);
     while !controller.state.exploded {
-        let pos = parse_input();
+        let pos = parse_input(width, height);
         let revealed = controller.reveal(pos);
         renderer.render(&revealed);
     }
 }
 
-fn parse_input() -> (i32, i32) {
+fn parse_input(max_x: i32, max_y: i32) -> (i32, i32) {
     println!("Enter position to reveal:");
     let mut input = String::new();
     io::stdin()
         .read_line(&mut input)
         .expect("Failed to read line");
-
-    let numbers: Vec<i32> = input
-        .trim()
+    let trimmed = input.trim();
+    if trimmed.is_empty() {
+        return parse_input(max_x, max_y);
+    }
+    let numbers: Vec<i32> = trimmed
         .split_whitespace()
         .map(|num| num.parse::<i32>().expect("Invalid number"))
         .collect();
+    let x = numbers[0];
+    let y = numbers[1];
     if numbers.len() != 2 {
         println!("Please enter exactly two numbers.");
-        parse_input()
+        parse_input(max_x, max_y)
+    } else if x >= max_x || y >= max_y || x < 0 || y < 0 {
+        println!("Stay within bounds {}x{}.", max_x, max_y);
+        parse_input(max_x, max_y)
     } else {
         (numbers[0], numbers[1])
     }
 }
+
 fn get_random_pos(width: i32, height: i32, rng: &mut rand::prelude::ThreadRng) -> (i32, i32) {
     (rng.gen_range(0..width), rng.gen_range(0..height))
 }
