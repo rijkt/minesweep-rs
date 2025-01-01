a59c7f81cb8fc5d0ec1d5fe3278f4029cffd894c
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2025-01-01 23:23:05 +0100

    introduce solver module

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use controller::{Controller, ControllerRequest, Process, RequestType};
+use controller::{Controller, Process};
 use rand::Rng;
 use render::{ConsoleRenderer, Render};
-use std::io;
+use solver::{PlayerSolver, Solve};
 mod board;
 mod controller;
 mod render;
@@ -12,40 +12,62 @@ fn main() {
     let mut controller = Controller::new(width, height);
     let renderer = ConsoleRenderer {};
     let mut rng = rand::thread_rng();
+    let mut solver = PlayerSolver {};
 
     renderer.render(&controller.state);
     while !controller.state.exploded {
-        let pos = parse_input(width, height);
-        let req: Vec<ControllerRequest> = vec![ControllerRequest{ req_type: RequestType::REVEAL, pos }];
+        let req = solver.solve(&controller.state);
         controller.process(req);
         renderer.render(&controller.state);
     }
 }
 
-fn parse_input(max_x: i32, max_y: i32) -> (i32, i32) {
-    println!("Enter position to reveal:");
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
-    let trimmed = input.trim();
-    if trimmed.is_empty() {
-        return parse_input(max_x, max_y);
+mod solver {
+    use std::io;
+
+    use crate::controller::{ControllerRequest, GameState, RequestType};
+
+    pub(crate) trait Solve {
+        fn solve(&mut self, game_state: &GameState) -> Vec<ControllerRequest>;
     }
-    let numbers: Vec<i32> = trimmed
-        .split_whitespace()
-        .map(|num| num.parse::<i32>().expect("Invalid number"))
-        .collect();
-    let x = numbers[0];
-    let y = numbers[1];
-    if numbers.len() != 2 {
-        println!("Please enter exactly two numbers.");
-        parse_input(max_x, max_y)
-    } else if x >= max_x || y >= max_y || x < 0 || y < 0 {
-        println!("Stay within bounds {}x{}.", max_x, max_y);
-        parse_input(max_x, max_y)
-    } else {
-        (numbers[0], numbers[1])
+
+    pub(crate) struct PlayerSolver {}
+
+    impl Solve for PlayerSolver {
+        fn solve(&mut self, game_state: &GameState) -> Vec<ControllerRequest> {
+            let pos = parse_input(game_state.width, game_state.height);
+            vec![ControllerRequest {
+                req_type: RequestType::REVEAL,
+                pos,
+            }]
+        }
+    }
+
+    fn parse_input(max_x: i32, max_y: i32) -> (i32, i32) {
+        println!("Enter position to reveal:");
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+        let trimmed = input.trim();
+        if trimmed.is_empty() {
+            return parse_input(max_x, max_y);
+        }
+        let numbers: Vec<i32> = trimmed
+            .split_whitespace()
+            .map(|num| num.parse::<i32>().expect("Invalid number"))
+            .collect();
+        let x = numbers[0];
+        let y = numbers[1];
+        if numbers.len() != 2 {
+            println!("Please enter exactly two numbers.");
+            parse_input(max_x, max_y)
+        } else if x >= max_x || y >= max_y || x < 0 || y < 0 {
+            println!("Stay within bounds {}x{}.", max_x, max_y);
+            parse_input(max_x, max_y)
+        } else {
+            (numbers[0], numbers[1])
+        }
     }
 }
 
