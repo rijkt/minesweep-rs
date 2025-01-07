011b716a7e7b4e869720567f277a6e00d266af68
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2025-01-07 23:29:55 +0100

    rewrite parsing for additional requests

--- src/solver/player_solver.rs
+++ src/solver/player_solver.rs
@@ -1,6 +1,6 @@
 use super::{PlayerSolver, Solve};
 use crate::controller::{ControllerRequest, GameState, RequestType};
-use std::io;
+use std::{io, vec};
 
 impl Solve for PlayerSolver {
     fn solve(&mut self, game_state: &GameState) -> Vec<ControllerRequest> {
@@ -9,13 +9,7 @@ impl Solve for PlayerSolver {
         if input.is_empty() {
             return vec![];
         }
-        let pos_opt = parse_pos(input.as_str(), game_state.width, game_state.height);
-        pos_opt.map_or(vec![], |pos| {
-            vec![ControllerRequest {
-                req_type: RequestType::Reveal,
-                pos,
-            }]
-        })
+        parse_request(game_state, input).map_or(vec![], |req| vec![req])
     }
 }
 
@@ -27,25 +21,36 @@ fn read_input() -> String {
     input.trim().to_owned()
 }
 
-fn parse_pos(input: &str, max_x: i32, max_y: i32) -> Option<(i32, i32)> {
-    if let Ok(numbers) = parse_nums(input) {
-        if numbers.len() != 2 {
-            println!("Please enter exactly two numbers.");
-            None
-        } else if numbers[0] >= max_x || numbers[1] >= max_y || numbers[0] < 0 || numbers[1] < 0 {
-            println!("Stay within bounds {max_x}x{max_y}.");
-            None
-        } else {
-            Some((numbers[0], numbers[1]))
+fn parse_request(game_state: &GameState, input: String) -> Option<ControllerRequest> {
+    let input_tokens: Vec<&str> = input.split_whitespace().collect();
+    match input_tokens.as_slice() {
+        [x, y] => {
+            parse_pos(x, y, game_state.width, game_state.height).map(|pos| ControllerRequest {
+                req_type: RequestType::Reveal,
+                pos,
+            })
         }
+        [prefix, x, y] => todo!(),
+        _ => {
+            println!("Please enter a valid command."); // TODO: specify
+            None
+        }
+    }
+}
+
+fn parse_pos(token1: &str, token2: &str, max_x: i32, max_y: i32) -> Option<(i32, i32)> {
+    if let (Ok(x), Ok(y)) = (token1.parse::<i32>(), token2.parse::<i32>()) {
+        validate_pos(x, y, max_x, max_y)
     } else {
         None
     }
 }
 
-fn parse_nums(input: &str) -> Result<Vec<i32>, std::num::ParseIntError> {
-    input
-        .split_whitespace()
-        .map(|num| num.parse::<i32>())
-        .collect()
+fn validate_pos(x: i32, y: i32, max_x: i32, max_y: i32) -> Option<(i32, i32)> {
+    if x >= max_x || y >= max_y || x < 0 || y < 0 {
+        println!("Stay within bounds {max_x}x{max_y}.");
+        None
+    } else {
+        Some((x, y))
+    }
 }
