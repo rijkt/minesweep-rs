b48313adb9181593daf0e4a68dceb0ddb08bde7a
Pascal Ellenberger <56129985+rijkt@users.noreply.github.com>
2025-02-02 21:30:12 +0100

    render coordinates around board (#11)
    * wip: render coordinates
    
    * use String
    
    * cleanup
    
    * add empty corner cell
    
    * extract fn
    
    * wip: styling
    
    * use builder functions
    
    * modify corner cell styling
    
    * differentiate board and label styles

--- src/render.rs
+++ src/render.rs
@@ -1,8 +1,12 @@
 use crate::controller;
 
 use super::controller::PlayTile;
+use itertools::Itertools;
 use tabled::{
-    settings::{object::Rows, Border, Style},
+    settings::{
+        object::{Cell, Columns, Rows},
+        Border, Modify, Style,
+    },
     Table,
 };
 
@@ -15,23 +19,19 @@ pub(crate) struct ConsoleRenderer {}
 impl Render for ConsoleRenderer {
     fn render(&self, state: &controller::GameState) {
         println!();
-        let view: Vec<Vec<&str>> = state
+        let rendered_board: Vec<Vec<String>> = state
             .board_view
             .iter()
             .map(|row| row.iter().map(render_tile).collect())
             .collect();
-        let mut table = Table::from_iter(view);
-        table.with(Style::ascii());
-        table.modify(
-            Rows::first(),
-            Border::inherit(Style::ascii())
-        );
+        let full_table_data = add_coordinate_labels(state, rendered_board);
+        let table = build_table(full_table_data);
         println!("{table}");
     }
 }
 
-fn render_tile(tile: &PlayTile) -> &str {
-    match tile {
+fn render_tile(tile: &PlayTile) -> String {
+    let rendered = match tile {
         PlayTile {
             pos: _,
             mine: true,
@@ -60,7 +60,8 @@ fn render_tile(tile: &PlayTile) -> &str {
             revealed: false,
             mine_neighbors: _,
         } => "🚩",
-    }
+    };
+    rendered.to_owned()
 }
 
 fn render_number_tile(num: &u8) -> &str {
@@ -76,4 +77,36 @@ fn render_number_tile(num: &u8) -> &str {
         8 => "8",
         _ => panic!("Encountered invalid neighbor count {num}"),
     }
+}
+
+fn add_coordinate_labels(
+    state: &controller::GameState,
+    view: Vec<Vec<String>>,
+) -> Vec<Vec<String>> {
+    let row_labels: Vec<String> = (0..state.height).map(|x| x.to_string()).collect_vec();
+    let with_row_labels = add_row_labels(&view, row_labels);
+    let col_labels: Vec<String> = (0..state.width).map(|x: i32| x.to_string()).collect_vec();
+    let leading_col_labels = std::iter::once("".to_owned()).chain(col_labels).collect(); // for empty corner
+    std::iter::once(leading_col_labels)
+        .chain(with_row_labels)
+        .collect::<Vec<Vec<String>>>()
+}
+
+fn add_row_labels(view: &[Vec<String>], row_labels: Vec<String>) -> Vec<Vec<String>> {
+    let mut augmented_data: Vec<Vec<String>> = Vec::new();
+    for (i, row) in view.iter().enumerate() {
+        let mut new_row = vec![row_labels[i].clone()]; // could be moved though
+        new_row.extend(row.clone());
+        augmented_data.push(new_row);
+    }
+    augmented_data
+}
+
+fn build_table(full_table: Vec<Vec<String>>) -> Table {
+    Table::from_iter(full_table)
+        .with(Style::extended())
+        .with(Modify::new(Rows::first()).with(Border::inherit(Style::ascii().top(' '))))
+        .with(Modify::new(Columns::first()).with(Border::inherit(Style::ascii().left(' '))))
+        .with(Modify::new(Cell::new(0, 0)).with(Border::inherit(Style::ascii().top(' ').left(' '))))
+        .to_owned()
 }
